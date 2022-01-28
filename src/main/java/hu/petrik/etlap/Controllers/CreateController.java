88644c3b9769850f6be1e5a03b07c07245c6f75b
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-01-28 20:41:31 +0100

    You can open the add form

--- src/main/java/hu/petrik/etlap/Controllers/CreateController.java
+++ src/main/java/hu/petrik/etlap/Controllers/CreateController.java
@@ -1,4 +1,19 @@
 package hu.petrik.etlap.Controllers;
 
-public class CreateController {
+import hu.petrik.etlap.Controller;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class CreateController extends Controller {
+    public TextField TFNev;
+    public TextField TFLeiras;
+    public Spinner SpinnerAr;
+    public ChoiceBox CBKategoria;
+    public Button ConfirmBtn;
+
+    public void onConfirmClicked(ActionEvent actionEvent) {
+    }
 }
