1662bbacc3f96966e468cc2e9db791735fc8b404
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-01-28 19:36:57 +0100

    The look of the form is finished

--- src/main/java/hu/petrik/etlap/Controllers/MainController.java
+++ src/main/java/hu/petrik/etlap/Controllers/MainController.java
@@ -80,14 +80,12 @@ public class MainController extends Controller {
 
     public void onSzazClicked(ActionEvent actionEvent) throws SQLException {
         int szazalek = 0;
-        try{
+        try {
             szazalek = szazalekEmeloSpinner.getValue();
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             alert("Nem lehet üres értékkel árat növelni");
             return;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             alert("0 vagy annál kisebb értékkel nem tudod növelni az árat");
             return;
         }
@@ -98,14 +96,12 @@ public class MainController extends Controller {
 
     public void onFtClicked(ActionEvent actionEvent) throws SQLException {
         int ft = 0;
-        try{
+        try {
             ft = FtEmeloSpinner.getValue();
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             alert("Nem lehet üres értékkel árat növelni");
             return;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             alert("0 vagy annál kisebb értékkel nem tudod növelni az árat");
             return;
         }
@@ -116,24 +112,33 @@ public class MainController extends Controller {
 
     public void onDeleteClicked(ActionEvent actionEvent) {
         int selectedIndex = EtlapTable.getSelectionModel().getSelectedIndex() + 1;
-        if (selectedIndex == 0){
+        if (selectedIndex == 0) {
             alert("Nem tudod a semmit kitörölni");
             return;
         }
 
         Etlap torlendoEtel = EtlapTable.getSelectionModel().getSelectedItem();
-        if (!confirm("Biztosan törölni szeretnéd ezt az ételt:" + torlendoEtel.getNev() + "?")){
+        if (!confirm("Biztosan törölni szeretnéd ezt az ételt:" + torlendoEtel.getNev() + "?")) {
             return;
-        }
-        else {
+        } else {
             try {
                 db.etelTorlese(selectedIndex);
                 alert("Sikeres törlés");
                 etlapListaFeltolt();
-            }
-            catch (SQLException e) {
+            } catch (SQLException e) {
                 alert("Sikertelen törlés");
             }
+        }
+    }
+
+    public void onInsertEtelClicked(ActionEvent actionEvent) {
+        try {
+            Controller insertAblak = ujAblak("create_etlap.fxml", "Étel hozzáadása", 300, 200);
+            insertAblak.getStage().setOnCloseRequest(windowEvent -> etlapListaFeltolt());
+            insertAblak.getStage().show();
+        }
+        catch (Exception e) {
+            hibaKiir(e);
         }
     }
 }
